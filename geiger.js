ae7e50553702a7fb93fad3930d013d9e8b706e58
bosadriaan <78053487+bosadriaan@users.noreply.github.com>
2023-08-31 18:30:56 -0300

    back cam

--- geiger.js
+++ geiger.js
@@ -38,6 +38,7 @@ function playVideo() {
       video: {
         width: { min: 640, ideal: 1280, max: 1920 },
         height: { min: 360, ideal: 720, max: 1080 },
+        facingMode: 'environment'
       },
       audio: false,
     })
